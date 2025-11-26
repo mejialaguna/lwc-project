70e62f29fe8e6d253b72063835b1f82f1af984c7
Jose Mejia <mejialaguna@yahoo.com>
2025-11-26 11:12:53 -0500

    LWC-components

--- force-app/main/default/lwc/mainLayout/mainLayout.js
+++ force-app/main/default/lwc/mainLayout/mainLayout.js
@@ -1,15 +1,17 @@
-import { LightningElement } from 'lwc';
+import { LightningElement } from "lwc";
 import { loadStyle } from "lightning/platformResourceLoader";
 import globalStyles from "@salesforce/resourceUrl/globalStyles";
 
 export default class communityLayout extends LightningElement {
-    /**
+  /**
    * Load only global CSS variables
    * @param {LightningElement} component - The component instance (pass 'this')
    * @param {globalStyles} staticFile - The file instance (pass 'global css file')
    * @returns {Promise<void>}
    */
-    connectedCallback() {
-      loadStyle(this, globalStyles);
-    }
+  connectedCallback() {
+    loadStyle(this, globalStyles).catch((error) => {
+      console.error("falied to load global styles", error);
+    });
+  }
 }
--- force-app/main/default/lwc/modalComponent/modalComponent.js
+++ force-app/main/default/lwc/modalComponent/modalComponent.js
@@ -1,7 +1,29 @@
 import { LightningElement, api } from "lwc";
+import { loadStyle } from "lightning/platformResourceLoader";
+import modalGlobalStyles from "@salesforce/resourceUrl/modalGlobalStyles";
 
 export default class C2pModalComponent extends LightningElement {
   @api shouldModalOpen;
+  hasCssLoaded = false;
+
+  /**
+   * Load only global CSS variables
+   * @param {LightningElement} component - The component instance (pass 'this')
+   * @param {globalStyles} staticFile - The file instance (pass 'global css file')
+   * @returns {Promise<void>}
+   */
+  connectedCallback() {
+    if (this.hasCssLoaded) return;
+
+    loadStyle(this, modalGlobalStyles)
+      .then(() => {
+        this.hasCssLoaded = true;
+      })
+      .catch((error) => {
+        console.error("falied to load modal global styles", error);
+      });
+  }
+
   handleModal() {
     // Dispatch event to parent
     const modalEvent = new CustomEvent("handlemodal", {
