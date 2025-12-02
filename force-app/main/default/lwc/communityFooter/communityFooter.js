fa0fe0fcac8a99e42c92e052801d42f48768791d
Jose Mejia <mejialaguna@yahoo.com>
2025-12-02 12:23:42 -0500

    update

--- force-app/main/default/lwc/communityFooter/communityFooter.js
+++ force-app/main/default/lwc/communityFooter/communityFooter.js
@@ -1,3 +1,34 @@
-import { LightningElement } from 'lwc';
+import { LightningElement } from "lwc";
 
-export default class CommunityFooter extends LightningElement {}
+export default class CommunityFooter extends LightningElement {
+  footerNode = null;
+
+  async connectedCallback() {
+    await this.getFooter();
+
+    const wrapper = this.template.querySelector(".wrapper");
+
+    if (wrapper && this.footerNode) {
+      wrapper.appendChild(this.footerNode);
+    }
+  }
+
+  async getFooter() {
+    try {
+      const res = await fetch("https://stage.fusion.inquirer.com");
+      const html = await res.text();
+
+      const parser = new DOMParser();
+      const doc = parser.parseFromString(html, "text/html");
+
+      const footer = doc.querySelector("footer");
+
+      // Store DOM node instead of an HTML string
+      this.footerNode = footer ? footer.cloneNode(true) : null;
+
+      console.log("Parsed footer:", this.footerNode);
+    } catch (err) {
+      console.error("Error fetching footer:", err);
+    }
+  }
+}
